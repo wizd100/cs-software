4597418c954b287800458c246fcd1b7e27ef42be
bkh <wizd100@gmail.com>
2021-09-13 12:35:59 +0900

    이스케이프

--- 1강/main.c
+++ 1강/main.c
@@ -2,15 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
-	int a;
-	int b;
-
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %d\n", a, b, a + b);
-	printf("%d - %d = %d\n", a, b, a - b);
-	printf("%d * %d = %d\n", a, b, a * b);
-	printf("%d / %d = %d\n", a, b, a / b);
-	printf("%d %% %d = %d\n", a, b, a % b);
+	printf("\"A\tB\nC");
 	system("pause");
 	return 0;
 }
