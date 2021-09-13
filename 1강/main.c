9f3a3f588435b637ddd128aaea3fd48df3d8907a
bkh <wizd100@gmail.com>
2021-09-13 12:42:04 +0900

    비교 연산자

--- 1강/main.c
+++ 1강/main.c
@@ -2,7 +2,19 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("\"A\tB\nC");
+	int a;
+	int b;
+	int c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", a > b); //true 1 false 0
+	printf("%d\n", a == b); //true 1 false 0
+	printf("%d\n", a < b); //true 1 false 0
+	printf("%d\n", a != b); //true 1 false 0
+	printf("%d\n", a && b); //true 1 false 0
+	printf("%d\n", a || b); //true 1 false 0
+
+
+
 	system("pause");
 	return 0;
 }
